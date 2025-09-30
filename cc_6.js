cf63a5f6ace6f7ab304a22587aa73e0ca5f8f4f2
Joshua Glaser <jdglaser@usf.edu>
2025-09-30 16:57:03 -0400

    completing step 3

--- cc_6.js
+++ cc_6.js
@@ -15,3 +15,20 @@ const Emp1 = new Employee("Josh", "Analyst")
 const Emp2 = new Employee("Jane", "Accounting")
 // Testing out step 2
 console.log(Emp1.describe())
+
+
+// Step 3 creating a subclass for manager
+class Manager extends Employee{
+    constructor (name,department,teamSize){
+        super(name,department)
+        this.teamSize = teamSize
+    }
+// This part describes the manager's role inheriting the name and department and adding the teamSize
+    describe(){
+        return `${this.name} is the manager of the ${this.department}. They oversee ${this.teamSize} people.`
+    }
+}
+// Testing oit step 3
+const Emp3 = new Manager("Ashley", "Marketing", 8 )
+
+console.log(Emp3.describe())
